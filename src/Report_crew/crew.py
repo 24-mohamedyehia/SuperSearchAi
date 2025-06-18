a343b0b059c70ac8f34ac31ad8e03b2b741ded32
mohamed-y <24-mohamedyehia>
2025-06-18 05:50:01 +0300

    add: implement ReportCrew class for deep search functionality

---
+++ src/Report_crew/crew.py
@@ -0,0 +1,43 @@
+from crewai import Agent, Crew, Process, Task 
+from crewai.project import CrewBase, agent, crew, task 
+from ..providers import deepseek_v3
+from ..custom_tools import read_json_tool
+import os
+
+@CrewBase
+class ReportCrew:
+    """
+    Report Crew for deep search about user query
+    """
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    @agent
+    def ReportAgent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['ReportAgent'],
+            allow_delegation=False,
+            verbose=True,
+            llm=deepseek_v3,
+            tools=[read_json_tool]
+        )
+    
+    @task
+    def ReportTask(self) -> Task:
+        return Task(
+            config=self.tasks_config['ReportTask'],
+            agent=self.ReportAgent(),
+            output_file = os.path.join(os.path.dirname(__file__), f"Final_Report/Deep_Research_Report.html")
+        )
+    
+    @crew
+    def crew(self) -> Crew:
+        """
+        Creates the research crew
+        """
+        return Crew(
+            agents=self.agents,  
+            tasks=self.tasks,  
+            process=Process.sequential,
+            verbose=True
+        )
