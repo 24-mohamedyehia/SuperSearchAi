c23e737961782a68d551a8441e9a4019f9e20e4c
mohamed-y <24-mohamedyehia>
2025-06-29 23:08:53 +0300

    refactor: rename QueryRefinerAgent and QueryRefinerTask to QueryGeneratorAgent and QueryGenerationTask for clarity

--- src/Research_crew/crew.py
+++ src/Research_crew/crew.py
@@ -2,32 +2,31 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task 
 from ..providers import mistral_small
 from .models import SuggestedSearchQueries
-from ..custom_tools import ask_user_tool, search_multiple_queries_tool, read_json_tool
+from ..custom_tools import search_multiple_queries_tool, read_json_tool
 import os
 
 @CrewBase
 class ResearchCrew:
     """
-    Research Crew for deep search about user query
+    Research Crew for quick search about user query
     """
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
 
     @agent
-    def QueryRefinerAgent(self) -> Agent:
+    def QueryGeneratorAgent(self) -> Agent:
         return Agent(
-            config=self.agents_config['QueryRefinerAgent'],
+            config=self.agents_config['QueryGeneratorAgent'],
             allow_delegation=False,
             verbose=True,
-            llm=mistral_small,
-            tools=[ask_user_tool]
+            llm=mistral_small
         )
     
     @task
-    def QueryRefinerTask(self) -> Task:
+    def QueryGenerationTask(self) -> Task:
         return Task(
-            config=self.tasks_config['QueryRefinerTask'],
-            agent=self.QueryRefinerAgent(),
+            config=self.tasks_config['QueryGenerationTask'],
+            agent=self.QueryGeneratorAgent(),
             output_json=SuggestedSearchQueries,
             output_file = os.path.join(os.path.dirname(__file__), f"research/step_one_search_queries.json")
         )
