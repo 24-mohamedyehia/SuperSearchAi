15bbe906a19a264ea032ca1c9744cd02cd594a57
mohamed-y <24-mohamedyehia>
2025-06-29 23:06:42 +0300

    feat: update app.py to include start, search, and results routers

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from src.routes import base_router, research_router
+from src.routes import base_router, start_router , search_router, results_router
+import agentops
+agentops.init()
 
 app = FastAPI()
 
 app.include_router(base_router)
-app.include_router(research_router)
+app.include_router(start_router)
+app.include_router(search_router)
+app.include_router(results_router)
+
