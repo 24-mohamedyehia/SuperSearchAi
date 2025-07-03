0175e86354a8d72247d19dfea7c4e870bb68f76d
mohamed-y <24-mohamedyehia>
2025-07-04 01:08:01 +0300

    refactor: update helper modules to improve structure and error handling

--- src/helpers/ask_user.py
+++ src/helpers/ask_user.py
@@ -1,62 +1,42 @@
-import requests
 import json
 from datetime import datetime
-from .config import get_settings
+from providers import get_deepseek_v3, InintLMM
+from helpers import load_prompt
+import os
 
-LLM_API_KEY= get_settings().LLM_API_KEY
+
+
+PORMPT_PATH = os.path.join(
+    os.path.dirname(__file__),      
+    "..",                           
+    "prompts",
+    "clarification_questions.yaml"
+)
+PORMPT_PATH = os.path.abspath(PORMPT_PATH)  
 current_date = datetime.now().strftime("%Y-%m-%d")
 
-def ask_user_for_clarification(user_question: str):
+def ask_user_for_clarification(user_question: str, llm_setting: InintLMM):
     """
     This function sends a request to the LLM to ask the user for clarification on their query.
     It formats the request with the current date and the user's question.
     """
+    deepseek_v3= get_deepseek_v3(llm_setting)
 
-    system_prompt = f"""
-    Receive this query from the user with The current date is {current_date}.
-
-    ### User Question:
-    {user_question}
-
-    ### Instructions:
-    - If it's unclear, ask clarifying questions for user to get more detail to get the best search results.
-    - ask all questions to user in one question. Don't ask more than 3 questions.
-    - Avoid asking repetitive or obvious questions.
-    - questions should be in numbered format.
-
-    ### Format your answer exactly like this (JSON):
-    {{
-    "clarification": [
-        {{ "question": "..." }},
-        {{ "question": "..." }},
-        {{ "question": "..." }}
-    ]
-    }}
-
-    Do NOT include any other text, only valid JSON.
-    Dont include any json format like ```json or ``` at the start or end of your response.
-    ### Answer:
-    """
-    response = requests.post(
-        url="https://openrouter.ai/api/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {LLM_API_KEY}",
-            "Content-Type": "application/json",
-        },
-        data=json.dumps({
-            "model": "deepseek/deepseek-chat-v3-0324:free",
-            "messages": [
-                {"role": "system", "content": system_prompt},
-            ],
-            "temperature": 0.5
-        })
+    system_prompt = load_prompt(
+    PORMPT_PATH,
+    current_date=current_date,
+    user_question=user_question
     )
-    
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+        {"role": "user", "content": system_prompt}
+    ]
+
     try:
-        raw_content = response.json()['choices'][0]['message']['content']
-        result = json.loads(raw_content)  
-        return result['clarification']
+        response = deepseek_v3.call(messages=messages)      # Make the LLM call with JSON response format
+        response = json.loads(response)                     # Parse the JSON response
+        return response['clarification']
     except Exception as e:
         print("❌ Error while parsing LLM response:", e)
-        return {"clarification": []}
-
+        return None
